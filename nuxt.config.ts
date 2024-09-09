29f1147b3c0294b973681b0de78cda2b948b7dd1
BBplayworld <bbplayworld@gmail.com>
2024-09-09 22:27:45 +0900

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-gtag'],
   app: {
     head: {
       title: 'BBstoqq: Real-Time Market Data, Economic Indicators, and Corporate News',
